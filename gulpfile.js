da55c419153a0d15293f24dccf33c844dff22179
Alex W <alex@overtureweb.com>
2017-12-31 17:07:18 -0800

    Initial Commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const uglify = require("gulp-uglify");
 
 gulp.task("default", () => {
     return gulp.src("script.js")
-        .pipe(babel())
+        .pipe(babel({
+            presets: ["env"]
+        }))
         .pipe(uglify())
         .pipe(gulp.dest("dist"));
 });

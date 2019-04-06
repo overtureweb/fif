3563c6fc950d3e6aa44b90171e36a7a2b36ea8d9
alexwarner <alex@overtureweb.com>
2019-04-06 10:35:50 -0700

    Rewrote using ES6

--- es6/script.js
+++ es6/script.js
@@ -1,22 +1,29 @@
-window.addEventListener('load', () => {
-    const btn = document.querySelector("#generateLetters");
-    btn.addEventListener("click", getLetters, false);
+(function () {
+    const container = document.querySelector("#container"),
+        pickLettersBtn = document.querySelector("#pickLetters");
 
-    function getLetters() {
-        const theLetters = [...document.querySelectorAll(".the-letter")];
-        let sequence = "aabbbcccdddeeffgghhhijjkllmmmnnopppqrrsssttuvwwyz**";
+    const handleClick = () => {
+        const items = ["a", "a", "b", "b", "b", "c", "c", "c", "d", "d", "d", "e", "e", "f", "f", "g", "g", "h", "h", "h", "i", "j", "j", "k", "l", "l", "m", "m", "m", "n", "n", "o", "p", "p", "p", "q", "r", "r", "s", "s", "s", "t", "t", "u", "v", "w", "w", "y", "z", "*", "*"];
 
-        theLetters.forEach((el) => {
-            let randNumber = Math.floor(Math.random() * sequence.length);
-            const aLetter = sequence.substr(randNumber, 1);
-            sequence = sequence.replace(aLetter, '');
-            el.innerHTML = ` ${aLetter}`;
+        const chosenItems = [...Array(5)].map(() => {
+            let i = Math.floor(Math.random() * items.length);
+            const item = items[i];
+            items.splice(i, 1);
+            return item;
         });
-    }
 
-    getLetters();
-});
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
 
+        chosenItems.forEach((el) => {
+            let newEl = document.createElement("h1");
+            newEl.innerHTML = el;
+            newEl.classList.add("mb-3");
+            container.appendChild(newEl);
+        });
+    };
 
-
+    pickLettersBtn.addEventListener("click", handleClick, true);
+})();
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 
-gulp.task("default", () => {
+gulp.task("babel", () => {
     return gulp.src("./es6/**/*.js")
         .pipe(babel({
             presets: ["env"]

9df550a3a592f672ad19be922b189afbdb743e53
alexwarner <alex@overtureweb.com>
2019-05-06 19:47:22 -0700

    Added header tags

--- es6/script.js
+++ es6/script.js
@@ -3,20 +3,23 @@
 		pickLettersBtn = document.querySelector("#pickLetters");
 
 	const handleClick = () => {
-		const items = ["a", "a", "b", "b", "b", "c", "c", "c", "d", "d", "d", "e", "e", "f", "f", "g", "g", "h", "h", "h", "i", "j", "j", "k", "l", "l", "m", "m", "m", "n", "n", "o", "p", "p", "p", "q", "r", "r", "s", "s", "s", "t", "t", "u", "v", "w", "w", "y", "z", "*", "*"];
+		const letters = ["a", "a", "b", "b", "b", "c", "c", "c", "d", "d", "d", "e", "e", "f", "f", "g", "g", "h", "h", "h", "i", "j", "j", "k", "l", "l", "m", "m", "m", "n", "n", "o", "p", "p", "p", "q", "r", "r", "s", "s", "s", "t", "t", "u", "v", "w", "w", "y", "z", "*", "*"];
 
-		const chosenItems = [...Array(5)].map(() => {
-			let i = Math.floor(Math.random() * items.length);
-			const item = items[i];
-			items.splice(i, 1);
+		//passing in a copy of the letters via currying makes this a pure function; no side effects and the original letter array is not mutated
+		const getLetters = (xLetters) => [...Array(5)].map(() => {
+			let i = Math.floor(Math.random() * xLetters.length);
+			const item = xLetters[i];
+			xLetters.splice(i, 1);
 			return item;
 		});
+
+		const chosenLetters = getLetters([...letters]);
 
 		while (container.hasChildNodes()) {
 			container.removeChild(container.firstChild);
 		}
 
-		chosenItems.forEach((el, i) => {
+		chosenLetters.forEach((el, i) => {
 			let newEl = document.createElement("h1");
 			newEl.innerHTML = el;
 			newEl.classList.add("mx-3", "letter", "d-inline-block");
